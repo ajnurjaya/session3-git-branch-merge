11563cc7fcf7a0878eb2049d340d71453fb89cd5
asep jajang nurjaya <ajnurjaya@gmail.com>
2020-12-02 13:54:18 +0700

    mengubah menjadi selamat sore

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 3000;
 const app = express();
 
 app.get('/',(req, res)=>{
-    res.send('Hello World');
+    res.send('Selamat sore');
 });
 
 app.listen(port, () => console.log(`hello world app listening on port ${port}!`));
